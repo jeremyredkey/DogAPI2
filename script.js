16ce80ffa2a7a7e07c3a3f0b1d46edb82148ef80
Jeremy Redkey <jeremy.redkey@gmail.com>
2020-07-27 21:35:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function getDogImage() {
+  let dogBreed = $('#dogBreed').val().toLowerCase()
+  console.log(dogBreed)
+  fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
+  $('.message').removeClass('hidden');
+}
+
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
